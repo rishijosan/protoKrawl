169524a386af782891f155e543742ebb33eed9ef
root <root@ron-VirtualBox.(none)>
2014-04-13 02:32:36 -0400

    Get Bars Returns JSON

--- getBars.py
+++ getBars.py
@@ -5,6 +5,7 @@ Created on Apr 12, 2014
 '''
 
 from flask import Flask
+from flask import request, jsonify
 import foursquare
 import json
 
@@ -20,19 +21,29 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route("/getVenue")
+@app.route("/getVenue" , methods=["GET", "POST"])
 def getVenue():
-    client = foursquare.Foursquare(client_id='ETWY3CHBMEFXU2DZZZ1DKTO2YPMNKJWNZTVSKBABBXTQEDW2', client_secret='X0H2FBGJ53ANMC23YHWQKI0LV5I52F12MWZMF5002EOJ0NSR')
-    ven  = client.venues('40a55d80f964a52020f31ee3')
-    srch = client.venues.search(ll='40.7,-74')
-    print srch
-    lat = 40.7
-    long = -74
     
-    with open(srch + 'srch.json', 'w') as outfile:
-        json.dump(ven, outfile)
+    if request.method == "GET":
         
-    return "Venue Dumped"
+        #reqParams = request.get_json()
+        reqParams = request.args
+        lat = reqParams['lat']
+        lon = reqParams['lon']
+        #print str(lat+', '+lon)
+    
+        client = foursquare.Foursquare(client_id='ETWY3CHBMEFXU2DZZZ1DKTO2YPMNKJWNZTVSKBABBXTQEDW2', client_secret='X0H2FBGJ53ANMC23YHWQKI0LV5I52F12MWZMF5002EOJ0NSR')
+        srch = client.venues.explore(params={'ll': str(lat+', '+lon), 'section' : 'drinks' , 'limit' : 10 })
+        locations = srch['groups'][0]['items']
+    
+        locList = dict()
+        n=0
+        for item in locations:
+            locList[n]=item['venue']
+            n+=1
+            
+        #result = json.dumps(locList)
+        return jsonify(locList)
     
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -6,12 +6,27 @@ Created on Apr 12, 2014
 
 import foursquare
 import json
-loc = '/media/sf_G_DRIVE/sharedWorkspace/protoKrawl/json/'
+loc = '/home/rishi/'
 
 client = foursquare.Foursquare(client_id='ETWY3CHBMEFXU2DZZZ1DKTO2YPMNKJWNZTVSKBABBXTQEDW2', client_secret='X0H2FBGJ53ANMC23YHWQKI0LV5I52F12MWZMF5002EOJ0NSR')
-srch = client.venues.search(params={'ll': '40.7,-74'})
+#srch = client.venues.search(params={'ll': '40.5212735, -74.4567546' , 'categoryId': '4d4b7105d754a06376d81259' , 'limit' : 10, 'radius' : 5000})
 #srch = client.venues.search(params={'query': 'coffee'})
-print srch
+#srch = client.venues.search(params={'near': 'Stony Brook'})
+#srch = client.venues.explore(params={'ll': '40.5212735, -74.4567546', 'limit' : 100, 'price' : [1,2,3] , 'day' : 6, })
+#srch = client.venues.explore(params={'ll': '40.5212735, -74.4567546', 'section' : 'drinks' , 'limit' : 10, 'price' : [1,2,3] , 'day' : 6 })
+
+srch = client.venues.explore(params={'ll': '40.5212735, -74.4567546', 'section' : 'drinks' , 'limit' : 10 })
+locations = srch['groups'][0]['items']
+
+locList = dict()
+n=0
+for item in locations:
+    locList[n]=item['venue']
+    n+=1
     
-with open(loc+'/srch.json', 'w') as outfile:
+result = json.dumps(locList)
+print srch
+
+    
+with open(loc+'drinks30.json', 'w') as outfile:
     json.dump(srch, outfile)
